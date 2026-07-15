7b5fff9f3080e200da2562aff423c967f3bece80
Nama Anda <email-anda@example.com>
2026-07-15 16:25:21 +0700

    fix: closing fragment tag in users table

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -807,6 +807,7 @@ export default function SettingsPage() {
                       ))}
                     </TableBody>
                   </Table>
+                  </>
                 )}
               </CardContent>
             </Card>
